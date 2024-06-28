153445025dadb59cb1db1cbf21b90af03eced1ed
sameer mina <85470158+s4m33r89@users.noreply.github.com>
2024-06-28 12:00:22 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -4,13 +4,14 @@ import "firebase/auth";
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyA4BrhBJdOBnksVnoYlLRd3kzyukvJIULw",
-    authDomain: "hdscreen-b299a.firebaseapp.com",
-    projectId: "hdscreen-b299a",
-    storageBucket: "hdscreen-b299a.appspot.com",
-    messagingSenderId: "548970071246",
-    appId: "1:548970071246:web:1e026b0bae867edec9bc9f",
-    measurementId: "G-NC6473VX1R"
+  apiKey: "AIzaSyC3LqjlslSztqXIyheVlrxrwQBLqZFzgF4",
+  authDomain: "playlab-c9b5c.firebaseapp.com",
+  databaseURL: "https://playlab-c9b5c-default-rtdb.firebaseio.com",
+  projectId: "playlab-c9b5c",
+  storageBucket: "playlab-c9b5c.appspot.com",
+  messagingSenderId: "889986164491",
+  appId: "1:889986164491:web:49cd5b1208e4e1ce68a40e",
+  measurementId: "G-G2ZZ531221"
 };
 
 const firebaseApp = firebase.initializeApp(config);
